f9e143f3612c5d1c9aa5ea7cfb1765dbf8f27ed2
Ranjan <63071934+Ranjankj@users.noreply.github.com>
2022-05-03 18:08:35 +0530

    Servlet Filter

---
+++ filterInServlet/IdFilter.java
@@ -0,0 +1,56 @@
+package com.ranjan;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class IdFilter
+ */
+@WebFilter("/profile")
+public class IdFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public IdFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		PrintWriter out = response.getWriter();
+		try {
+			int id = Integer.parseInt(request.getParameter("id"));
+			chain.doFilter(request, response);
+		}catch(Exception e)
+		{
+			out.println("Invalid ID!");
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
